fed88ffeec762f4947b1c9595f26e6e86d9ee40f
Piotr Skowron <skowronpiotrmail@gmail.com>
2024-04-07 22:51:29 +0200

    Adding code to views

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
+
+
+def scrape(request):
+    page = requests.get('http://www.facebook.com')
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    link_address = []
+
+    for link in soup.find_all('a'):
+        link_address.append(link.get('href'))
+
+    return render(request, 'myapp/result.html', {'link_address': link_address})
